6ca9271c2bbf16c512e23d4a39708e1e9c2062c6
azetor <adam.a.szymczak@gmail.com>
2018-03-11 11:05:08 +0100

    api added mock test

--- api/src/test/java/com/mobilemonkeysoftware/api/GithubApiTest.kt
+++ api/src/test/java/com/mobilemonkeysoftware/api/GithubApiTest.kt
@@ -1,7 +1,13 @@
 package com.mobilemonkeysoftware.api
 
 import com.google.gson.JsonSyntaxException
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Observable
 import it.cosenonjaviste.daggermock.InjectFromComponent
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -19,7 +25,10 @@ class GithubApiTest {
 
     @Before
     fun setup() {
+    }
 
+    @After
+    fun clean() {
     }
 
     @Test
@@ -42,4 +51,24 @@ class GithubApiTest {
                 .assertError(JsonSyntaxException::class.java)
                 .assertSubscribed()
     }
+
+    @Test
+    fun testNoValues() {
+
+        var apiMock: GithubApi = mock() // PRO TIP
+        val emptyListObservable: Observable<List<GithubApi.Gist>> = Observable.empty()
+
+//        doReturn(emptyListObservable).`when`(apiMock).getPublicGistOk()
+        whenever(apiMock.getPublicGistOk()).doReturn(emptyListObservable)
+
+        apiMock
+                .getPublicGistOk()
+                .test()
+                .assertNoValues()
+                .assertNoErrors()
+                .assertSubscribed()
+
+        verify(apiMock).getPublicGistOk()
+    }
+
 }
